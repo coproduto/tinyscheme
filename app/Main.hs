13dfc30b8d4010a8b8fde7a2e0a98507ea63df03
Pedro Castilho <cstl.px@gmail.com>
2016-07-06 22:17:52 -0300

    Single term parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,59 @@
 module Main where
 
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+
+data LispVal = LispAtom String
+             | LispList [LispVal]
+             | LispDottedList [LispVal] LispVal
+             | LispNumber Integer
+             | LispString String
+             | LispBool Bool
+               deriving Show
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0)
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
+
+readExpr :: String -> String
+readExpr input = 
+    case parse parseExpr "lisp" input of
+      Left err -> "No match: " ++ show err
+      Right val -> "Found value: " ++ show val
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+            <|> parseString
+            <|> parseNumber
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ LispString x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+             "#t" -> LispBool True
+             "#f" -> LispBool False
+             _    -> LispAtom atom
+
+parseNumber :: Parser LispVal
+parseNumber = do
+  digits <- many1 digit
+  return $ (LispNumber . read) digits
+
+symbol :: Parser Char
+symbol = 
+    oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = 
+    skipMany1 space
